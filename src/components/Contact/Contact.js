0f4a0e3f051891304f25bbc613f163ec1cd8b036
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-16 18:57:20 +0600

    Final Design With Email Section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Contact.css';
 import Walmart from '../../assets/walmart.png';
 import Adobe from '../../assets/adobe.png';
@@ -8,36 +8,62 @@ import FaceBookIcon from '../../assets/facebook-icon.png';
 import InstraIcon from '../../assets/instagram.png';
 import TwitterIcon from '../../assets/twitter.png';
 import YoutubeIcon from '../../assets/youtube.png';
+import emailjs from '@emailjs/browser';
 
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_pzv7kci', 'template_ivkaxus', form.current, 'nn8L7XlSiaAU__svm9Kyr')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }; 
+
   return (
    <section id='contactPage'>
+
     <div id="cilents"> 
+
        <h1 className="contactPageTitle">My Clients</h1>
+
        <p className="clientDesc">
         I Have had the opportunity to work with a diverse group of companies.
         Some of the notable companies I have work includes
        </p>
+
        <div className="clientImgs">
+
         <img src={Walmart} alt="Walmart" className="clientImg" />
         <img src={Adobe} alt="Adobe" className="clientImg" />
         <img src={MicroSoft} alt="MicroSoft" className="clientImg" />
         <img src={FaceBook} alt="FaceBook" className="clientImg" />
+
        </div>
     </div>
     <div id="contact">
+
        <h1 className="contactPageTitle">Contact Me</h1>
+
        <span className="contactDesc">Please fill out the form the below
         to discuss any work opportunities</span>
-        <form className='contactForm'>
-            <input type="text" className='name' placeholder='Your Name' />
-            <input type="text" className="email"  placeholder='Your Email' />
-            <textarea  className='desc' row='10' placeholder='Your Message' />
+
+        <form className='contactForm' ref={form}  onSubmit={sendEmail}>
+
+            <input type="text" className='name' placeholder='Your Name' name='your_name'/>
+            <input type="text" className="email"  placeholder='Your Email' name='your_email' />
+            <textarea  className='desc' row='10' name='message' placeholder='Your Message' />
            
         </form>
         <button type='submit' value='send' className='btn'>Submit</button>
+
         <div className="links">
+
             <img src={FaceBookIcon} alt="" className="link" />
             <img src={InstraIcon} alt="" className="link" />
             <img src={TwitterIcon} alt="" className="link" />
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-scroll';
 import contactImg from '../../assets/contact.png';
+import menu from '../../assets/menu.png';
+import emailjs from '@emailjs/browser';
 
 
 const Navbar = () => {
+  
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
    <nav className='navbar'>
     <img src={logo} alt="Logo" className='logo' />
@@ -15,11 +20,21 @@ const Navbar = () => {
       <Link  activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuItemList'>Portfolio</Link>
       <Link  activeClass='active' to='contactPage' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuItemList'>Clients</Link>
     </div>
+
     <button className='desktopMenuBtn' onClick={() =>{
       document.getElementById('contact').scrollIntoView('smooth')
     }}>
         <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me
     </button>
+
+    <img src={menu} alt="menu" className='mobMenu' onClick={() => setShowMenu(!showMenu )} />
+    <div className="navMenu" style={{display: showMenu? 'flex' : 'none'}}>
+      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='ItemList' onClick={() => setShowMenu(false)}>Home</Link>
+      <Link  activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='ItemList' onClick={() => setShowMenu(false)}>About</Link>
+      <Link  activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='ItemList'onClick={() => setShowMenu(false)}>Portfolio</Link>
+      <Link  activeClass='active' to='contactPage' spy={true} smooth={true} offset={-50} duration={500} className='ItemList' onClick={() => setShowMenu(false)}>Clients</Link>
+      <Link  activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className='ItemList' onClick={() => setShowMenu(false)}>Contact</Link>
+    </div>
    </nav>
   )
 }
