f13e6b813e76881dfd02800fb5a875f4f11aab94
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-16 15:11:41 +0600

    Navbar Design Complte

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,12 +10,14 @@ const Navbar = () => {
    <nav className='navbar'>
     <img src={logo} alt="Logo" className='logo' />
     <div className="desktopMenu">
-      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} className='desktopMenuItemList'>Home</Link>
-      <Link className='desktopMenuItemList'>About</Link>
-      <Link className='desktopMenuItemList'>Portfolio</Link>
-      <Link className='desktopMenuItemList'>Clients</Link>
+      <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuItemList'>Home</Link>
+      <Link  activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuItemList'>About</Link>
+      <Link  activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuItemList'>Portfolio</Link>
+      <Link  activeClass='active' to='contactPage' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuItemList'>Clients</Link>
     </div>
-    <button className='desktopMenuBtn'>
+    <button className='desktopMenuBtn' onClick={() =>{
+      document.getElementById('contact').scrollIntoView('smooth')
+    }}>
         <img src={contactImg} alt="" className="desktopMenuImg" />Contact Me
     </button>
    </nav>
